6fb3144c3b4e5dcb72ab6f6e1b05478d2685cac3
HugoPoletto34 <hugopoletto34@gmail.com>
2021-01-04 12:06:37 -0300

    Structure

---
+++ Backend/src/main/java/com/poletto/dpoletto/DpolettoApplication.java
@@ -0,0 +1,13 @@
+package com.poletto.dpoletto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DpolettoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DpolettoApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/poletto/dpoletto/DpolettoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poletto.dpoletto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DpolettoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
