20a9d8b74e53bfb4d273b60aa353233279177951
HugoPoletto34 <hugopoletto34@gmail.com>
2021-01-05 10:18:00 -0300

    PUT /Orders/id/delivered

--- Backend/src/main/java/com/poletto/dpoletto/services/OrderService.java
+++ Backend/src/main/java/com/poletto/dpoletto/services/OrderService.java
@@ -1,5 +1,6 @@
 package com.poletto.dpoletto.services;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -8,18 +9,44 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.poletto.dpoletto.dto.OrderDTO;
+import com.poletto.dpoletto.dto.ProductDTO;
 import com.poletto.dpoletto.entities.Order;
+import com.poletto.dpoletto.entities.OrderStatus;
+import com.poletto.dpoletto.entities.Product;
 import com.poletto.dpoletto.repositories.OrderRepository;
+import com.poletto.dpoletto.repositories.ProductRepository;
 
 @Service
 public class OrderService {
 	
 	@Autowired
 	private OrderRepository repository;
+	
+	@Autowired
+	private ProductRepository productRepository;
 
 	@Transactional (readOnly = true)
 	public List <OrderDTO> findAll () {
 		List<Order> list = repository.findOrderWithProducts();
 		return list.stream().map(x -> new OrderDTO (x)).collect(Collectors.toList());
+	}
+	
+	@Transactional
+	public OrderDTO insert (OrderDTO dto) {
+		Order order = new Order(null, dto.getAddress(), dto.getLatitude(), dto.getLongitude(), Instant.now(), OrderStatus.PENDING);
+		for (ProductDTO p : dto.getProducts()) {
+			Product product = productRepository.getOne(p.getId());
+			order.getProducts().add(product);
+		}
+		order = repository.save(order);
+		return new OrderDTO(order);
+	}
+	
+	@Transactional
+	public OrderDTO setDelivered (Long id) {
+		Order order = repository.getOne(id);
+		order.setStatus(OrderStatus.DELIVERED);
+		order = repository.save(order);
+		return new OrderDTO(order);
 	}
 }
