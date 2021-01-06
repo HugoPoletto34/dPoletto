37c1f54c9b25959918707c868d16a9e79d81fdf8
HugoPoletto34 <hugopoletto34@gmail.com>
2021-01-06 19:53:44 -0300

    Add Routes and StepsHeader

--- front-web/src/NavBar/index.tsx
+++ front-web/src/NavBar/index.tsx
@@ -1,11 +1,12 @@
 import './styles.css'
 import { ReactComponent as Logo}  from './logo.svg'
+import { Link } from 'react-router-dom';
 
 function NavBar () {
     return (
         <nav className="main-navbar">
             <Logo />
-            <a href="home" className="logo-text">DPoletto</a>
+            <Link to="/" className="logo-text">DPoletto</Link>
         </nav>
     )
 }
