757562e6e0d6def50b88796fe2a82bbbb050ddb4
HugoPoletto34 <hugopoletto34@gmail.com>
2021-01-09 21:48:10 -0300

    Revert "LocalHost to API-Heroku"
    This reverts commit cdeaff97622fc0bb416722a9678a3a20d6c89437.

--- front-web/src/api.ts
+++ front-web/src/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = 'https://poletto-sds2.herokuapp.com'
+const API_URL = 'http://localhost:8080'
 export function  fetchProducts () {
     return axios(`${API_URL}/products`)
 }
