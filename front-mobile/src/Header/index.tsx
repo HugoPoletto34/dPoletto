9fa1f88a12720550308cd81c1baaec9c2ab93fc2
HugoPoletto34 <hugopoletto34@gmail.com>
2021-01-13 20:03:32 -0300

    Create Routes

--- front-mobile/src/Header/index.tsx
+++ front-mobile/src/Header/index.tsx
@@ -1,12 +1,21 @@
+import { useNavigation } from '@react-navigation/native';
 import React from 'react';
 import { Image, StyleSheet, Text, View } from 'react-native';
+import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
 
 export default function Header() {
-  return (
-    <View style={styles.container}>
-        <Image source={require('../assets/logo.png')} />
-        <Text style={styles.text}>DPoletto</Text>
-    </View>
+    const navigation = useNavigation();
+
+    const handleOnPress = () => {
+        navigation.navigate('Home');
+    }
+    return (
+      <TouchableWithoutFeedback onPress={handleOnPress}> 
+        <View style={styles.container}>
+            <Image source={require('../assets/logo.png')} />
+            <Text style={styles.text}>DPoletto</Text>
+        </View>
+      </TouchableWithoutFeedback>
   );
 }
 
